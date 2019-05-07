aa407a9b45d32d9236a253a241d65608c122afe3
Leo Q <leoq91@gmail.com>
2019-05-06 20:00:02 -0700

    add post routes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,8 +8,8 @@ class Home extends Component {
   state = {
     posts: [],
     title: '',
-    author: '',
-    synopsis: '',
+    body: '',
+    category: '',
   };
 
   componentDidMount() {
@@ -24,11 +24,51 @@ class Home extends Component {
       .catch(err => console.log(err));
   };
 
+  onChange = key => e => this.setState({ [key]: e.target.value });
+
+  onClick = () =>
+    API.createPost({
+      title: this.state.title,
+      body: this.state.body,
+      category: this.state.category,
+    }).then(() =>
+      this.setState({
+        posts: [
+          ...this.state.posts,
+          {
+            title: this.state.title,
+            body: this.state.body,
+            category: this.state.category,
+          },
+        ],
+      })
+    );
   render() {
     return (
       <div>
         {JSON.stringify(this.state.posts)}
         <RandomHomeComponent />
+        <div>
+          <input
+            type="text"
+            value={this.state.title}
+            label="title"
+            onChange={this.onChange('title')}
+          />
+          <input
+            type="text"
+            value={this.state.body}
+            label="body"
+            onChange={this.onChange('body')}
+          />
+          <input
+            type="text"
+            value={this.state.category}
+            label="category"
+            onChange={this.onChange('category')}
+          />
+          <button onClick={this.onClick}>Create</button>
+        </div>
       </div>
     );
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,6 +5,9 @@ export default {
   getPosts: function() {
     return axios.get('/api/posts');
   },
+  createPost: function(data) {
+    return axios.post('/api/posts', data);
+  },
   // // Gets the book with the given id
   // getBook: function(id) {
   //   return axios.get("/api/books/" + id);
